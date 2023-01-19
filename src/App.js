67b007b3506d0ab53807484c02f081771bdd0076
Wilfred <wilfredcloudspace@gmail.com>
2023-01-19 17:23:34 +0100

    Change how components being imported

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
-import Navbar from './component/Navbar'
-import Feed from './component/Feed'
-import VideoDetail from './component/VideoDetail'
-import ChannelDetail from './component/ChannelDetail'
-import SearchFeed from './component/SearchFeed'
 import { Box } from '@mui/material'
+
+import {Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed} from './component'
+
 
 const App = () => (
   <BrowserRouter>
-    <Box sx={{backgroudColor: '#000'}}>
+    <Box sx={{backgroundColor: '#000'}}>
       <Navbar />
       <Routes>
         <Route path='/' element={<Feed />} />
