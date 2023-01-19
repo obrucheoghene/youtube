a1d3e0fd278bfa3aee7f158aeb2ca771cfc87a4e
Wilfred <wilfredcloudspace@gmail.com>
2023-01-19 15:22:55 +0100

    Create app component files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+import Navbar from './component/Navbar'
+import Feed from './component/Feed'
+import VideoDetail from './component/VideoDetail'
+import ChannelDetail from './component/ChannelDetail'
+import SearchFeed from './component/SearchFeed'
+import { Box } from '@mui/material'
+
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{backgroudColor: '#000'}}>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Feed />} />
+        <Route path='video/:id' element={<VideoDetail />} />
+        <Route path='channel/:id' element={<ChannelDetail />} />
+        <Route path='search/:searchTerm' element={<SearchFeed />} />
+
+      </Routes>
+    </Box>
+
+  </BrowserRouter>
+)
 
 export default App
