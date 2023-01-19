c3a8088737235a721c1dfa50f155b1e984f3161b
Wilfred <wilfredcloudspace@gmail.com>
2023-01-19 22:12:35 +0100

    Add logo and searchbar to Navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Stack } from '@mui/material'
+import { Link } from 'react-router-dom'
 
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+import { Logo } from '../utils/contants'
+import SearchBar from './SearchBar'
+
+const Navbar = () => (
+    <Stack direction="row" 
+    alignItems="center"
+    justifyContent="space-between" 
+    sx={{ position: 'sticky', background: '#000', top: '0'}}>
+
+        <Link to="/" style={{ alignItems: 'center', display: 'flex' }}>
+            <img src={Logo} alt="Logo" height="45"/>
+        </Link>
+
+        <SearchBar/>
+    </Stack>
+)
 
 export default Navbar
