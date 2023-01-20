6cea386c7bb39fb8cd8f951e7f0b6360dddcb009
Wilfred <wilfredcloudspace@gmail.com>
2023-01-20 19:01:15 +0100

    Build channeldetail page

--- src/component/ChannelCard.jsx
+++ src/component/ChannelCard.jsx
@@ -3,7 +3,7 @@ import { CheckCircle } from '@mui/icons-material'
 import { Link } from 'react-router-dom'
 import { demoProfilePicture } from '../utils/contants'
 
-const ChannelCard = ({channelDetail}) => (
+const ChannelCard = ({channelDetail, marginTop}) => (
   <Box sx={{boxShadow: 'none',
   borderRadius: '20px',
   display: 'flex',
@@ -11,7 +11,8 @@ const ChannelCard = ({channelDetail}) => (
   alignItems: 'center',
   width: {xs: '356px', md: '320px'},
   height: '326px',
-  margin: 'auto'}}>
+  margin: 'auto',
+  marginTop}}>
 
 <Link to={`/channel/${channelDetail?.id?.channelId}`}>
   <CardContent sx={{display: 'flex', 
@@ -30,7 +31,7 @@ const ChannelCard = ({channelDetail}) => (
       {channelDetail?.statistics?.subscriberCount && (
         <Typography>
           {parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString()}
-          Subscribers
+           <span style={{marginLeft: '5px'}}>Subscribers</span>
         </Typography>
       )}
   </CardContent>
--- src/component/ChannelDetail.jsx
+++ src/component/ChannelDetail.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
+import {useParams} from 'react-router-dom'
+import {Box} from '@mui/material'
+import {Videos, ChannelCard} from './'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
 
 const ChannelDetail = () => {
+  const [ChannelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+  const {id} = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`)
+    .then((data) => setChannelDetail(data?.items[0]));
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
+    .then((data) => setVideos(data?.items));
+  }, [id])
   return (
-    <div>ChannelDetail</div>
+    <Box minHeight="95vh">
+      <Box>
+        <div style={{backgroundColor: '#8EC5FC',
+backgroundImage: 'linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%)',
+zIndex: 10,
+height: '300px'
+}}/>
+<ChannelCard channelDetail={ChannelDetail} marginTop="-110px" />
+      </Box>
+
+      <Box display="flex" p="2">
+        <Box sx={{mr: {sm: '100px'}}} />
+        <Videos videos={videos} />
+
+      </Box>
+    </Box>
   )
 }
 
