4b698d5c47e04dc9a6bf33258d8e004306a4131d
Wilfred <wilfredcloudspace@gmail.com>
2023-01-20 11:02:12 +0100

    Integrate Rapid API

--- src/component/Feed.jsx
+++ src/component/Feed.jsx
@@ -1,14 +1,20 @@
 import {useState, useEffect} from 'react'
 import {Box, Stack, Typography,  } from '@mui/material'
-import SideBar from './SideBar'
+import {SideBar, Videos} from './'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState('New')
+  useEffect(()=>{
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+  }, [selectedCategory])
   return (
     <Stack direction={{ xs: 'column', md: 'row'}}>
 
       <Box sx={{ height: {xs: 'auto', md: '92vh'}, 
       borderRight: '1px solid #3d3d3d', px: {xs: 0, md: 2}}}>
-        <SideBar/>
+        <SideBar selectedCategory={selectedCategory} 
+        setSelectedCategory={setSelectedCategory}/>
 
         <Typography className="copyright" 
         variant="body2"
@@ -17,7 +23,17 @@ const Feed = () => {
         </Typography> 
       </Box>
 
+    <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex: 2}}>
+      <Typography variant="h4"
+      fontWeight="bold" mb={2} sx={{color: 'white'}}>
+        {selectedCategory} <span style={{color: '#F31503'}}>videos</span>
+
+      </Typography>
+
+      <Videos videos={[]}/>
+    </Box>
     </Stack>
+
   )
 }
 
--- src/component/SideBar.jsx
+++ src/component/SideBar.jsx
@@ -2,15 +2,16 @@ import {useState, useEffect} from 'react'
 import { Stack } from '@mui/material'
 import { categories } from '../utils/contants'
 
-const selectedCategory = "New";
 
-const SideBar = () => (
+const SideBar = ({selectedCategory, setSelectedCategory}) => (
     <Stack direction={{ xs: 'row', md: 'column'}}
     sx={{overflowY: 'auto', 
     height: {xs: 'auto', md: '95%'} }}>
 
         {categories.map((category) => (
-            <button className="category-btn"
+            <button 
+            className="category-btn"
+            onClick={() => setSelectedCategory(category.name)}
             style={{ background: category.name === selectedCategory 
             && '#FC1503', color: 'white' }}>
 
