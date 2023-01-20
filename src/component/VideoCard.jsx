744c4b71301ff78f318b450f99dc00b9bf33fa35
Wilfred <wilfredcloudspace@gmail.com>
2023-01-20 14:50:25 +0100

    Build ChannelCard component

--- src/component/VideoCard.jsx
+++ src/component/VideoCard.jsx
@@ -11,7 +11,7 @@ import { demoThumbnailUrl, demoVideoUrl,
 const VideoCard = ({video: {id: {videoId}, snippet}}) => {
   return (
     <Card sx={{width: {xs: '100%', md: '320px'}, 
-    boxShadow: 'none', borderRadius: 'none'}}>
+    boxShadow: 'none', borderRadius: 0}}>
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl} >
       <CardMedia image={snippet?.thumbnails?.high?.url} 
       alt={snippet?.title}
@@ -26,11 +26,11 @@ const VideoCard = ({video: {id: {videoId}, snippet}}) => {
             {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0,60)}
           </Typography>
           </Link>
-
           <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoVideoUrl}>
           <Typography variant="subtitle2"
           fontWeight="bold" color="gray">
             {snippet?.channelTitle || demoChannelTitle}
+            <CheckCircle sx={{fontSize: 12, color: 'gray', ml: '5px'}} />
           </Typography>
           </Link>
       </CardContent>
